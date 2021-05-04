2eb79083560cc248cd82ea9c26091334efafedea
pdchinh <pdc1761.n@gmail.com>
2021-05-04 16:54:34 +0700

    start

---
+++ src/main/java/com/vachiShop/controller/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.vachiShop.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("HomeControllerOfWeb")
+public class HomeController {
+	
+	 @RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+	   public ModelAndView homePage() {
+	      ModelAndView mav = new ModelAndView("web/home");
+	      return mav;
+	   }
+
+	 
+}
